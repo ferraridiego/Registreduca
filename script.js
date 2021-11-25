15b0f171f482e90e2494191dbfbc5f0fb4c19bc2
ferraridiego <ferrari.diego92@gmail.com>
2021-11-24 21:30:48 -0300

    pagina de login

---
+++ script.js
@@ -0,0 +1,27 @@
+/*const fizzBuzz = (entrada) => {
+    if(typeof entrada != 'number'){
+        return 'Not a Number'
+    } else if (entrada % 5 === 0 && entrada % 3 === 0){
+        return 'FizzBuzz'
+    } else if (entrada % 3 === 0){
+        return 'Fizz'
+    } else if (entrada % 5 === 0){
+        return 'Buzz'
+    } else {
+        return entrada
+    }
+}
+
+const resultado = fizzBuzz(30);
+console.log(resultado) */
+
+//######################### Map ##########################
+/*const numbers = [1,2,3,4]
+
+const squareNumbers = numbers.map(arr => arr ** 2) 
+
+console.log(squareNumbers); */
+
+//######################### Reduce ##########################
+
+console.log('Olá Diego')
